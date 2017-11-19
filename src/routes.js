11c86e4f1b94f9927a711526b7eddbd800fbf7a0
Aoi <rungruangsap.aoi@g.swu.ac.th>
2017-11-19 15:26:33 +0700

    my first react

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Main from './pages/Main'
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route exact path='/' component={Main} />
+            <Route exact path='/login' component={Login} />
+        </Switch>
+    )
+}
+export default Routes
